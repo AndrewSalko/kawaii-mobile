1560447338eafadbdf7e27232dac183435a4b5fe
AndrewSalko <andrewsalko@gmail.com>
2015-01-13 10:41:56 +0200

    Добавлена генерация карты для изображений. Но нужно переделать, т.к.в корне сайта нет прав создавать эти файлы.

--- www/wp-content/plugins/addthis-kawaii/addthis-kawaii.php
+++ www/wp-content/plugins/addthis-kawaii/addthis-kawaii.php
@@ -7,6 +7,8 @@ Author: <a href="http://www.salkodev.com/">Andrew Salko</a>
 Description: A helper AddThis plugin for a <a href="http://kawaii-mobile.com">http://kawaii-mobile.com</a>
 */
 
+require_once(plugin_dir_path( __FILE__ ) . 'kawaii-sitemap.php');
+
 if (!class_exists("KawaiiAddThis")) 
 {
 	class KawaiiAddThis
@@ -79,6 +81,83 @@ if (!class_exists("KawaiiAddThis"))
 			return  $content . $addContent;
 		}
 
+		function do_image_sitemap()
+		{
+    		if(function_exists('add_submenu_page')) 
+			{
+				add_submenu_page('tools.php', 'Image Sitemap Kawaii', 'Image Sitemap Kawaii', 'manage_options', 'image-sitemap-kawaii-uniq-id', array('KawaiiAddThis', 'do_image_sitemap_generate'));
+			}
+		}
+
+		function do_image_sitemap_generate() 
+		{
+			if ($_POST ['submit']) 
+			{
+				$result=KawaiiAddThis::Generate();
+
+			    if (!$result)
+				{
+					echo '<br /><div class="error"><h2>An error occured!</h2><p>The XML sitemap was generated successfully but the  plugin was unable to save the xml to your WordPress root folder at <strong>' . $_SERVER["DOCUMENT_ROOT"] . '</strong>.</p><p>Please ensure that the folder has appropriate <a href="http://codex.wordpress.org/Changing_File_Permissions" target="_blank">write permissions</a>.</p><p> You can either use the chmod command in Unix or use your FTP Manager to change the permission of the folder to 0666 and then try generating the sitemap again.</p><p>If the issue remains unresolved, please post the error message in this <a target="_blank" href="http://wordpress.org/tags/google-image-sitemap?forum_id=10#postform">WordPress forum</a>.</p></div>';
+					exit();
+				}
+				else
+				{
+					echo '<h2>Done</h2>';
+				}
+
+			}
+			else
+			{
+				echo '<div class="wrap">';
+				echo '<div style="width:800px; padding:10px 20px; background-color:#eee; font-size:.95em; font-family:Georgia;margin:20px">';
+  				echo '<h2>XML Sitemap for Images (kawaii-mobile)</h2>';
+  				echo '<p>Sitemaps are a way to tell Google and other search engines about web pages, images and video content on your site that they may otherwise not discover. </p>';
+				echo '<form id="options_form" method="post" action="">';
+    			echo '<div class="submit">';
+      			echo '<input type="submit" name="submit" id="sb_submit" value="Generate Image Sitemap" />';
+    			echo '</div>';
+				echo '</form>';
+				echo '<p>Click the button above to generate a Image Sitemap for your website. Once you have created your Sitemap, you should submit it to Google using Webmaster Tools. </p>';
+				echo '</div>';
+			}//else
+
+		}//do_image_sitemap_generate
+
+		public static function IsImageSitemapWritable($filename)
+		{
+			if(!is_writable($filename))
+			{
+				if(!@chmod($filename, 0666))
+				{
+					$pathtofilename = dirname($filename);
+					if(!is_writable($pathtofilename))
+					{
+						if(!@chmod($pathtofilename, 0666))
+						{
+							return false;
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public static function Generate()
+		{
+			$srvRoot=$_SERVER["DOCUMENT_ROOT"];
+			if(KawaiiAddThis::IsImageSitemapWritable($srvRoot)===false)
+			{
+				return false;
+			}
+
+			//return an array fileName=>XML body
+			$xmlFiles=KawaiiSiteMap::Generate($srvRoot);
+			if($xmlFiles===false)
+				return false;
+
+			return true;
+		}//Generate
+
 
 	}//class
 
@@ -87,7 +166,7 @@ if (!class_exists("KawaiiAddThis"))
 		$pluginKawaiiAddThis = new KawaiiAddThis();
 	}
 
-} //End Class KawaiiAddThis
+}//End Class KawaiiAddThis
 
 //Actions and Filters	
 if (isset($pluginKawaiiAddThis)) 
@@ -97,4 +176,6 @@ if (isset($pluginKawaiiAddThis))
 	add_action('wp_enqueue_scripts', array('KawaiiAddThis','do_add_stylesheet'), 1);
 
 	add_filter('the_content', array('KawaiiAddThis', 'do_content'),1);
+
+	add_action('admin_menu', array('KawaiiAddThis', 'do_image_sitemap'),1);
 }
