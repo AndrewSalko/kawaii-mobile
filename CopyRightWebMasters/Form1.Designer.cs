9f39ca58513925d6920aa5f02a24710152a5d83a
AndrewSalko <andrewsalko@gmail.com>
2017-01-17 19:13:56 +0200

    Add tool to extract a lot of URLS from DMCA message (google webmasters)

---
+++ CopyRightWebMasters/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace CopyRightWebMasters
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._ButtonStart = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// _ButtonStart
+			// 
+			this._ButtonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this._ButtonStart.Location = new System.Drawing.Point(345, 147);
+			this._ButtonStart.Name = "_ButtonStart";
+			this._ButtonStart.Size = new System.Drawing.Size(112, 23);
+			this._ButtonStart.TabIndex = 0;
+			this._ButtonStart.Text = "Start";
+			this._ButtonStart.UseVisualStyleBackColor = true;
+			this._ButtonStart.Click += new System.EventHandler(this._ButtonStart_Click);
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(445, 56);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Из файла source.htm извлекает ссылки на сайт, которые были блокированы, и формиру" +
+    "ет результат-массив для js";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(469, 182);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this._ButtonStart);
+			this.Name = "Form1";
+			this.Text = "Извлечение ссылок из сообщения WebMasters";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button _ButtonStart;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ CopyRightWebMasters/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CopyRightWebMasters
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void _ButtonStart_Click(object sender, EventArgs e)
+		{
+			//target="_blank">http://kawaii-mobile.com/2016/01/photo-kano/</a>
+
+			string srcFileName = Path.Combine(Application.StartupPath, "source.htm");
+			
+			if(!File.Exists(srcFileName))
+			{
+				MessageBox.Show(this, "Файл " + srcFileName + " не найден", "Файл исходных данных не найден", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				return;
+			}
+
+			string allText = File.ReadAllText(srcFileName);
+
+			string firstBlock = "break-all;\" target=\"_blank\">";
+			string lastBlock = "</a>";
+			string pattern = firstBlock + "(.*?)" + lastBlock;
+
+			List<string> resultURLs = new List<string>();
+
+			Regex rg = new Regex(pattern);
+			MatchCollection matches = rg.Matches(allText);
+			if (matches != null)
+			{
+				for (int i = 0; i < matches.Count; i++)
+				{
+					Match m = matches[i];
+					string result = m.Value;
+
+					if (string.IsNullOrWhiteSpace(result))
+						continue;
+
+					result= result.Replace(lastBlock, string.Empty);
+					result = result.Replace(firstBlock, string.Empty);
+
+					string url = string.Format("\"{0}\"", result);
+					resultURLs.Add(url);
+				}
+			}
+
+			string resFileName = Path.Combine(Application.StartupPath, "result.txt");
+
+			using (var resFile = File.CreateText(resFileName))
+			{
+				//теперь делаем для массива:			
+				for (int j = 0; j < resultURLs.Count; j++)
+				{
+					string resLine = resultURLs[j];
+
+					if (j != resultURLs.Count - 1)
+					{
+						resLine += ",";
+					}
+
+					resFile.WriteLine(resLine);
+				}
+			}
+
+			MessageBox.Show(this, "Done", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+	}
+}
