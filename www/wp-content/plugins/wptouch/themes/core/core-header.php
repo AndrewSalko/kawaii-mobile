b23ba0facb7c9df5e714dbaa1f51a63d4182e213
AndrewSalko <andrewsalko@gmail.com>
2017-05-20 12:14:07 +0300

    Для моб.режима показ баннеров активирован (добавил в хед-часть загрузку скрипта adsense)

--- www/wp-content/plugins/wptouch/themes/core/core-header.php
+++ www/wp-content/plugins/wptouch/themes/core/core-header.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-2908292943805064",
+    enable_page_level_ads: true
+  });
+</script>
 	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
