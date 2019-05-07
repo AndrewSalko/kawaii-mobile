794281d201f02a5e4e5f0e4ac2aab4b98f20b057
AndrewSalko <andrewsalko@gmail.com>
2019-05-07 22:00:37 +0300

    Added "By genres" shortcode plugin

---
+++ www/wp-content/plugins/shortcuts-kawaii/kawaii-genres.php
@@ -0,0 +1,129 @@
+<?php
+
+// Get urls to posts by anime genres
+class KawaiiGenres
+{
+	public $GenreTags = array(
+	'action',
+	'adventure',
+	'alternate-history',
+
+	'comedy',
+	'comedy-drama',
+	'black-comedy',	//!!!
+
+	'crime-fiction',
+	'cyberpunk',
+
+
+	'detective',
+	'drama',
+	'dystopian',
+	'ecchi',
+
+	'educational',
+
+	'fantasy',
+	'dark-fantasy',
+	'science-fantasy',
+	'urban-fantasy',
+
+	'girls-with-guns',
+
+	'harem',
+	'historical',
+	'horror',
+
+	'japanese-idol',
+
+
+	'magical-girl',
+	'magic',
+
+	'martial-arts',
+
+	'mecha',
+
+	'military',
+	'military-science-fiction',
+
+
+	'mystery',
+
+	'murder',
+	'noir',
+
+	'parody',
+	'paranormal',
+	'post-apocalyptic',
+	'police',
+
+	'professional-wrestling',
+
+	'psychological',
+	'psychological-thriller',
+	'psychology',
+	'social-psychology',
+
+
+	'romance',
+	'romantic-comedy',
+
+	'sci-fi',
+
+	'science-fiction',	// !!!
+	'sci-fi-noir',
+
+	'school-story',
+	'slice-of-life',
+	'steampunk',
+	'space-western',
+	'sports',
+
+	'spy',
+	'suspense',
+
+	'survival-action',	// !!!
+	'survival',
+	'survival-horror',	// !!!
+
+	'supernatural',
+
+	'thriller',
+	'tragedy',
+
+	'visual-novel',
+
+	'war-drama'
+	);
+
+
+	public function GetGenres($siteUrl)
+	{
+		$resultText='';
+
+		foreach ($this->GenreTags as $genreTagSlug)
+		{
+			$genreName=$genreTagSlug;
+
+			$term = get_term_by('slug', $genreTagSlug, 'post_tag');
+
+		
+			if(!($term===false))
+			{
+				$genreName=$term->name;			
+				$genreName .= ' ('.$term->count.')';
+			}
+
+			$linkBlock='<p><a href="'.$siteUrl.'/tag/'.$genreTagSlug.'/">'.$genreName.'</a></p>';
+
+
+			$resultText .= $linkBlock;
+		}
+
+		return $resultText;
+	}
+
+
+
+}
---
+++ www/wp-content/plugins/shortcuts-kawaii/shortcuts-kawaii.php
@@ -0,0 +1,48 @@
+<?php
+/* 
+Plugin Name: Shortcuts Kawaii
+Plugin URI: https://kawaii-mobile.com/
+Version: v1.00
+Author: <a href="http://kawaii-mobile.com/">Andrew Salko</a>
+Description: A helper shortcuts plugin for a <a href="http://kawaii-mobile.com">http://kawaii-mobile.com</a>
+*/
+
+require_once(plugin_dir_path( __FILE__ ) . 'shortcuts-kawaii.php');
+
+if (!class_exists("ShortcutsKawaii")) 
+{
+	include ('kawaii-genres.php');
+
+	class ShortcutsKawaii
+	{
+
+		function ShortcutsKawaii() 
+		{ //constructor			
+		}
+
+		function do_shortcode_genres($atts, $content) 
+		{
+			$siteURL=get_site_url();
+
+			$genres=new KawaiiGenres();
+			$resultLinks=$genres->GetGenres($siteURL);
+
+			return $resultLinks;
+		}
+
+
+	}//class
+
+	if (class_exists("ShortcutsKawaii")) 
+	{
+		$pluginShortcutsKawaii = new ShortcutsKawaii();
+	}
+
+}//End Class ShortcutsKawaii
+
+//Actions and Filters	
+if (isset($pluginShortcutsKawaii)) 
+{    			
+	add_shortcode('kawaii-genres', array('ShortcutsKawaii', 'do_shortcode_genres'));
+}
+
