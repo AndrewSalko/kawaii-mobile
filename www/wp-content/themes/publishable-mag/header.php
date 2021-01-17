858fc51db25290d132b7ccd06422887ce3b0e73a
Andrew Salko <andrewsalko@gmail.com>
2021-01-17 11:44:31 +0200

    rel=preload для шрифта publishable-mag.woff2

--- www/wp-content/themes/publishable-mag/header.php
+++ www/wp-content/themes/publishable-mag/header.php
@@ -16,6 +16,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="preload" href="/wp-content/themes/publishable-mag/fonts/publishable-mag.woff2" as="font" type="font/woff2" crossorigin>
 	<?php wp_head(); ?>
 </head>
 
